40276e44427c2889463de8788937e083a55596ba
Gustavo Colombelli <gustavocolombelli@gmail.com>
2024-11-21 18:22:53 -0300

    adicionado arquivo referente as funcionalidades principais do taskBoard

---
+++ scripts/pages/taskBoard.js
@@ -0,0 +1,54 @@
+import { API_BASE_URL } from "../../config/apiConfig.js";
+import { getFromLocalStorage } from "../utils/storage.js";
+
+const boardsList = document.getElementById("boardsList");
+const userNameSpan = document.getElementById("userName");
+const logoutButton = document.getElementById("logoutButton");
+
+async function loadBoards() {
+    try {
+        const response = await fetch(`${API_BASE_URL}/Boards`);
+        if (!response.ok) {
+            throw new Error("Erro ao carregar boards");
+        }
+        const boards = await response.json();
+        populateBoardsDropdown(boards);
+    } catch (error) {
+        console.error("Erro ao carregar boards:", error);
+    }
+}
+
+
+function populateBoardsDropdown(boards) {
+    console.log(boards)
+    boards.forEach((board) => {
+        console.log(board)
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<a class="dropdown-item" value="${board.Id}">${board.Name}</a>`;
+        boardsList.appendChild(listItem);
+    });
+}
+
+
+function loadUserName() {
+    const userName = getFromLocalStorage("user");
+    console.log(userName)
+    if (userName.name) {
+        userNameSpan.textContent = `Olá, ${userName.name.split(' ')[0]}`;
+    } else {
+        userNameSpan.textContent = "Usuário não identificado";
+    }
+}
+
+logoutButton.addEventListener("click", () => {
+    localStorage.removeItem("user");
+    window.location.href = "index.html";
+});
+
+
+function init() {
+    loadUserName();
+    loadBoards();
+}
+
+init();
