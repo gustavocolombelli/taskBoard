f3c00b63b99dfbd35b8ec0791bc087ac765ad05a
brunoluizs <y.bruno@live.com>
2024-11-21 21:39:33 -0300

    adicionado carregamento das colunas

--- scripts/pages/taskBoard.js
+++ scripts/pages/taskBoard.js
@@ -4,6 +4,8 @@ import { getFromLocalStorage } from "../utils/storage.js";
 const boardsList = document.getElementById("boardsList");
 const userNameSpan = document.getElementById("userName");
 const logoutButton = document.getElementById("logoutButton");
+const boardTitle = document.getElementById("boardTitle");
+const boardLayout = document.getElementById("board");
 
 async function loadBoards() {
     try {
@@ -19,13 +21,42 @@ async function loadBoards() {
 }
 
 function populateBoardsDropdown(boards) {
-    console.log(boards)
+    
     boards.forEach((board) => {
-        console.log(board)
         const listItem = document.createElement("li");
-        listItem.innerHTML = `<a class="dropdown-item" value="${board.Id}">${board.Name}</a>`;
+        listItem.innerHTML = `<a class="dropdown-item" id="dropdown-item" value="${board.Id}">${board.Name}</a>`;
+        listItem.addEventListener("click", (event) => {
+            // console.log(board.Id)
+            boardTitle.innerHTML = event.target.innerHTML;
+            loadBoard(board.Id);
+        })
         boardsList.appendChild(listItem);
     });
+}
+
+async function loadBoard(id) {
+    try {
+        const response = await fetch(`${API_BASE_URL}/ColumnByBoardId?BoardId=${id}`)
+        if(!response.ok) {
+            throw new Error("Erro ao carregar colunas");
+        }
+        const columns = await response.json();
+        populateColumns(columns);
+    } catch (error) {
+        console.error("Erro ao carregar colunas:", error);
+    }
+}
+
+function populateColumns(columns) {
+    boardLayout.innerHTML = "";
+
+
+    columns.forEach((column) => {
+        const columnItem = document.createElement("article");
+        columnItem.className = "column-item"
+        columnItem.innerHTML = `<h5>${column.Name}</h5>`
+        boardLayout.appendChild(columnItem);
+    })
 }
 
 function loadUserName() {
